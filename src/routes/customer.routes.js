5c4b87717534b3ea3876c6800d759b38836dba9a
ChickenTTO <datchutich69@gmail.com>
2026-01-04 02:43:49 +0700

    â

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -6,30 +6,40 @@ const {
   getTripsByPhone,
   updateCustomer
 } = require('../controllers/customer.controller');
-const { protect } = require('../middleware/auth');
-const { authorize } = require('../middleware/rbac');
-const { USER_ROLES } = require('../utils/constants');
-const { createCustomer } = require('../controllers/customer.controller');
 
-router.post(
-  '/',
-  authorize(USER_ROLES.ADMIN, USER_ROLES.DISPATCHER),
-  createCustomer
-);
-
-
-router.use(protect);
-
-// All authenticated users
+// Lấy danh sách khách hàng
 router.get('/', getCustomers);
+
+// Lấy chi tiết khách hàng theo ID
 router.get('/:id', getCustomerById);
+
+// Lấy lịch sử chuyến đi theo số điện thoại
 router.get('/phone/:phone/trips', getTripsByPhone);
 
-// Admin và Dispatcher
-router.put(
-  '/:id',
-  authorize(USER_ROLES.ADMIN, USER_ROLES.DISPATCHER),
-  updateCustomer
-);
+// Thêm khách mới (POST)
+router.post('/', async (req, res, next) => {
+  try {
+    const Customer = require('../models/Customer');
+    const newCustomer = await Customer.create(req.body);
+    res.status(201).json(newCustomer);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Cập nhật khách hàng (PUT)
+router.put('/:id', updateCustomer);
+
+// Xóa khách hàng (DELETE)
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const Customer = require('../models/Customer');
+    const deleted = await Customer.findByIdAndDelete(req.params.id);
+    if (!deleted) return res.status(404).json({ message: 'Không tìm thấy khách hàng' });
+    res.json({ message: 'Xóa thành công' });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
