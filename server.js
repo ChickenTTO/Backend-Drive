df0f360c5d2e6c5cb9a6feb44be279dbbd655bb7
ChickenTTO <datchutich69@gmail.com>
2026-01-03 22:19:20 +0700

    add cros

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/database');
 
+const cors = require('cors'); // Import thư viện
+const express = require('express');
+const app = express();
+
+app.use(cors()); 
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 // Kết nối database
