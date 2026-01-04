23bc044645a4b6e0bfd6417ee8f54f4e4cf30487
ChickenTTO <datchutich69@gmail.com>
2026-01-04 17:55:53 +0700

    fix vite

--- src/app.js
+++ src/app.js
@@ -20,7 +20,11 @@ const app = express();
 
 // --- MIDDLEWARE ---
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: 'https://driver-git-main-chickenttos-projects.vercel.app', // Frontend Vercel
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    credentials: true // nếu dùng cookie/token
+}));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
